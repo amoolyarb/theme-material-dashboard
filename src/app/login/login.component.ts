848949743ff01014d64bc202afa9fb531c15a4c7
nisha.max <nisha.bharadwaj@medhand.net>
2020-01-09 18:01:38 +0530

    removed lint errors

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss','vendor/bootstrap/css/bootstrap.min.css','fonts/font-awesome-4.7.0/css/font-awesome.min.css','fonts/Linearicons-Free-v1.0.0/icon-font.min.css','vendor/animate/animate.css','vendor/css-hamburgers/hamburgers.min.css','vendor/animsition/css/animsition.min.css','vendor/select2/select2.min.css','vendor/daterangepicker/daterangepicker.css','css/util.css','css/main.css']
+  styleUrls: ['./login.component.scss', 'vendor/bootstrap/css/bootstrap.min.css',
+  'fonts/font-awesome-4.7.0/css/font-awesome.min.css', 'fonts/Linearicons-Free-v1.0.0/icon-font.min.css',
+  'vendor/animate/animate.css', 'vendor/css-hamburgers/hamburgers.min.css',
+  'vendor/animsition/css/animsition.min.css', 'vendor/select2/select2.min.css',
+  'vendor/daterangepicker/daterangepicker.css', 'css/util.css', 'css/main.css']
 })
 export class LoginComponent implements OnInit {
 
@@ -11,5 +15,4 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
