1895a4d2bc8842ba10159d60d7c0e4ab85b4edf4
nisha.max <nisha.bharadwaj@medhand.net>
2020-01-09 17:17:52 +0530

    modeified version 1 of signup and login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,6 +6,7 @@ import { LandingComponent } from './landing/landing.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes =[
   { path: '', component: LandingComponent },
@@ -18,7 +19,8 @@ const routes: Routes =[
     }]
   },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component:SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent }
 ];
 
 @NgModule({
