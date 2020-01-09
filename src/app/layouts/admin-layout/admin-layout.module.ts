34ad30a57a9a6962892309f77023825f3eafbea2
amoolyarb <amoolya.bayari@medhand.net>
2020-01-09 12:44:43 +0530

    landing page added

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -12,7 +12,7 @@ import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import { TestsComponent } from '../../tests/tests.component';
-
+import { LandingComponent } from '../../landing/landing.component';
 
 import {
   MatButtonModule,
@@ -44,7 +44,8 @@ import {
     MapsComponent,
     NotificationsComponent,
     UpgradeComponent,
-    TestsComponent
+    TestsComponent,
+    LandingComponent
   ]
 })
 
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -9,7 +9,7 @@ import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import { TestsComponent } from '../../tests/tests.component';
-
+import { LandingComponent } from '../../landing/landing.component';
 
 export const AdminLayoutRoutes: Routes = [
     // {
@@ -63,4 +63,5 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'notifications',  component: NotificationsComponent },
     { path: 'upgrade',        component: UpgradeComponent },
     { path: 'tests',        component: TestsComponent },
+    { path: 'landing',        component: LandingComponent },
 ];
